9a65dbface2aeb2102616b862dcf8fb376390052
HIND <HIND@DESKTOP-4622SJB>
2021-03-01 19:33:31 +0100

    add a model:user class

--- src/test/main.java
+++ src/test/main.java
@@ -3,8 +3,8 @@ package test;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("ceci est un projet hhhh");
-	
+		System.out.println("ceci est un test hhhh");
+		System.out.println("ceci est un 2eme test hhhh");
 		
 	}
 
